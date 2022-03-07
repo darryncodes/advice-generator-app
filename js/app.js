b054d2fb3d14baf5d245b6178d3c534faa591477
Darryn Codes <hello@darryncodes.co.uk>
2022-03-07 16:03:04 +0000

    functionality - creating logic for api and function for diabling the button and the loader

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const number = document.querySelector('.slip__number');
+const quote = document.querySelector('.slip__quote');
+const btn = document.querySelector('.btn');
+const loader = document.querySelector('.loader');
+
+async function fetchAdvice() {
+  const response = await fetch('https://api.adviceslip.com/advice');
+  const advice = response.json();
+
+  return advice;
+}
+
+async function handleClick() {
+  const { slip } = await fetchAdvice();
+
+  number.textContent = slip.id;
+  quote.textContent = slip.advice;
+
+  loader.classList.remove('hidden');
+  btn.disabled = true;
+
+  setTimeout(function () {
+    loader.classList.add('hidden');
+    btn.disabled = false;
+  }, 2000);
+}
+
+btn.addEventListener('click', handleClick);
